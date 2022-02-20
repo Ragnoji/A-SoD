b9b2ac3c823d7f92930c1203a289190482e97f45
Ragnoji <58487417+Ragnoji@users.noreply.github.com>
2022-02-20 11:02:23 +0300

    Added queue

---
+++ 1HomeWork/singlequeu.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class SingleQueue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def enqueue(self, value):
+        element = Node(value)
+        element.next = self.head
+        self.head = element
+        if self.length == 0:
+            self.tail = self.head
+        self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        prev = self.head
+        current = prev.next
+        self.length -= 1
+        if current is None:
+            self.head = None
+            self.tail = None
+            return prev
+        while current.next is not None:
+            prev = prev.next
+            current = current.next
+        prev.next = None
+        self.tail = prev
+        return current
+
+    def check(self):
+        return self.tail.value
+
+    def length(self):
+        return self.length
